4d992ab1338285c9651c2013dcc87f969b8809a9
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-26 17:05:52 +0200

    finished gui

--- src/ui/index.js
+++ src/ui/index.js
@@ -6,24 +6,23 @@ google.charts.setOnLoadCallback(drawChart);
 //code for drawing charts
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work', 8],
-    ['Eat', 2],
-    ['TV', 4],
-    ['Gym', 2],
-    ['Sleep', 8]
-  ]);
+        ['Gas', 'Percentage'],
+        ['LPG', 1.6],
+        ['Propan', 1.6],
+        ['Butan', 1.6],
+        ['Metan', 1.6],
+        ['Vodík', 1.6],
+        ["Vzduch", 2]
+    ]);
   
     // Optional; add a title and set the width and height of the chart
     var options = {
         width:690, 
         height:400, 
-        backgroundColor: 'transparent', 
-        textStyle:{
-            color: 'white',
-            fontSize: 30
-        },
-        is3D: true
+        backgroundColor: 'transparent',
+        legend: 'none',
+        is3D: true,
+        slices: {0: {color: '#7e3b3a'}, 1:{color: '#7e703a'}, 2:{color: '#5a7e3a'}, 3: {color: '#3a7e6b'}, 4:{color: '#3c3a7e'}}
     };
   
     // Display the chart inside the <div> element with id="piechart"
