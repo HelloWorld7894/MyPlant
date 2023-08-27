acc4e2f61e1ec6a73cc7df8806ef1526ae7f9141
HelloWorld7894 <danpojhan@protonmail.com>
2023-08-27 23:27:12 +0200

    added led blinking

--- pico/main.py
+++ pico/main.py
@@ -1,8 +1,7 @@
 import select
 import sys
 import time
-from machine import Pin, ADC
-import utime
+from machine import ADC, Pin
 
 # Set up the poll object
 poll_obj = select.poll()
@@ -15,20 +14,21 @@ led = Pin(25, Pin.OUT)
 
 # Loop indefinitely
 while True:
-    
+    led.toggle()
+    time.sleep(1)
+    led.toggle()
+
     # Wait for input on stdin
     poll_results = poll_obj.poll(1) # the '1' is how long it will wait for message before looping again (in microseconds)
     if poll_results:
-        
         # Read the data from stdin (read data coming from PC)
-        data = sys.stdin.readline().decode("utf-8")
-        if data == "read":
+        data = sys.stdin.readline().strip()
+        if data == "read-temp":
             reading = sensor_temp.read_u16() * conversion_factor
-            temperature = round(27 - (reading - 0.706)/0.001721)
-            
-            
-            sys.stdout.write("received data: " + str(temperature) + "\r")
-        else:
-            sys.stdout.write("received data: " + data + "\r")
+            temperature = str(round(27 - (reading - 0.706)/0.001721, 2))
+        
+        sys.stdout.write("%s" % temperature)
     else:
         led.toggle()
+        # do something if no message received (like feed a watchdog timer)
+        continue
